942d0c2b4e42f791041ff87aedecb5b0f848158b
dp3why <dp3why@gmail.com>
2024-02-18 15:52:09 -0800

    con

--- constants/constants.go
+++ constants/constants.go
@@ -11,7 +11,7 @@ var _ = dotenv.Load()
 var ES_AWS_PASSWORD string = os.Getenv("ES_AWS_PASSWORD")
 var ES_AWS_URL string = os.Getenv("ES_AWS_URL")
 var ES_USERNAME string= os.Getenv("ES_USERNAME")
-var GCS_BUCKET string = os.Getenv("GCS_BUCKET")
+var GCS_BUCKET string = os.Getenv("GCS_BUCKET_NAME")
 var PRIVATE_KEY string = os.Getenv("GCS_PRIVATE_KEY")
 var CLIENT_EMAIL string = os.Getenv("GCS_CLIENT_EMAIL")
 
